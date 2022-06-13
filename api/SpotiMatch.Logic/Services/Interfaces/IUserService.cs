646a9ac0f89084fdd2a386ea2f507beec8c7db06
Alexandre COSTA MOREIRA <alexcostamoreira.pro@gmail.com>
2022-06-13 23:37:52 +0200

    Add logic

---
+++ api/SpotiMatch.Logic/Services/Interfaces/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SpotiMatch.Shared.Dtos;
+
+namespace SpotiMatch.Logic.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDto>> GetUsers(CancellationToken cancellationToken = default);
+
+        Task<UserDto> GetUser(CancellationToken cancellationToken = default);
+
+        Task<UserDto> AddUser(CancellationToken cancellationToken = default);
+
+        Task<UserDto> UpdateUser(CancellationToken cancellationToken = default);
+
+        Task<bool> DeleteUser(CancellationToken cancellationToken = default);
+    }
+}
---
+++ api/SpotiMatch.Logic/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SpotiMatch.Logic.Services.Interfaces;
+using SpotiMatch.Database.Repositories.Interfaces;
+using System.Threading.Tasks;
+using SpotiMatch.Shared.Dtos;
+using System.Threading;
+
+namespace SpotiMatch.Logic.Services
+{
+    public class UserService: IUserService
+    {
+        private IUserRepository UserRepository { get; }
+
+        public UserService(IUserRepository userRepository)
+        {
+            UserRepository = userRepository;
+        }
+
+        public Task<IEnumerable<UserDto>> GetUsers(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> GetUser(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> AddUser(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> UpdateUser(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUser(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
