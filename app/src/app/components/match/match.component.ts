230a4db1edc89604eeff4dec52b43c35244915de
Pololasticocotier <paul.simonella@gmail.com>
2022-06-20 00:27:52 +0200

    [ 🩹 / 💄  ]  Fixing bug and low perf with the dominant color

--- app/src/app/components/match/match.component.ts
+++ app/src/app/components/match/match.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { average } from 'color.js'
 import { CdkDragDrop, copyArrayItem, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { async } from 'rxjs';
 
 @Component({
   selector: 'app-match',
@@ -9,24 +10,33 @@ import { CdkDragDrop, copyArrayItem, moveItemInArray, transferArrayItem } from '
 })
 export class MatchComponent implements OnInit {
 
-  colorDominant: any;
+  userColorDominant: any;
+  otherUserColorDominant: any;
   showText: boolean = false;
   num = 60;
   options:number = 0;
+  style: any;
 
 
   constructor() { }
 
   ngOnInit(): void {
-    average(this.friendlist[0].img,{ format: 'hex' }).then( x => {
-      this.colorDominant =  x ;
-      document.documentElement.style.setProperty("colorDominant",this.colorDominant);
+    average('https://i.scdn.co/image/ab6775700000ee857b6e5a4ffdf90095c68ae386',{ format: 'hex' }).then( colorHexa => {
+      this.userColorDominant =  colorHexa ;
     },);
   }
 
-  setMyStyles() {
+  userSetMyStyles() {
     let styles = {
-      'background': 'linear-gradient(180deg,' + this.colorDominant + ' 0%, rgba(12,12,12,1) 100%)',
+      'background': 'linear-gradient(180deg,' + this.userColorDominant + ' 0%, rgba(12,12,12,1) 100%)',
+    };
+    return styles;
+  }
+
+  otherUserSetMyStyles() {
+    console.log(this.otherUserColorDominant);
+    let styles = {
+      'background': 'linear-gradient(180deg,' + this.otherUserColorDominant + ' 0%, rgba(12,12,12,1) 100%)',
     };
     return styles;
   }
@@ -44,13 +54,11 @@ export class MatchComponent implements OnInit {
     }
   }
 
-  friendlist = [{name:'Paul',age:'14', img:'https://pbs.twimg.com/media/E0o2L4iWUAUiA9I.jpg'}];
+  friendlist = [{name:'Pedro',age:'14', img:'https://pbs.twimg.com/media/E0o2L4iWUAUiA9I.jpg'},{name:'Paul',age:'14', img:'https://i.scdn.co/image/ab6775700000ee857b6e5a4ffdf90095c68ae386'}];
 
   target: any[] = [];
 
   drop(event: CdkDragDrop<any[]>){
-
-    
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -71,6 +79,10 @@ export class MatchComponent implements OnInit {
           event.currentIndex,
         );
       }
+      average(this.target[0].img,{ format: 'hex' }).then( colorHexa => {
+        this.otherUserColorDominant =  colorHexa ;
+        this.style = this.otherUserSetMyStyles();
+      },);
     }
   }
 
