319ddf4b6e7f4be7e18e756aabf9447d20a757e5
Fyleek <odecloquement@gmail.com>
2022-06-19 22:29:20 +0200

    [✨/💚/] Update Spotimatch Services

--- app/src/app/api.service.ts
+++ app/src/app/api.service.ts
@@ -2,7 +2,7 @@ import { registerData } from './model/register.model';
 import { loginData } from './model/login.model';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, Observable, Subscription, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from './model/user';
 import { Auth } from './model/auth.model';
@@ -16,49 +16,67 @@ export class ApiService {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type':  'application/json',
-      Authorization: 'my-auth-token'
+      Authorization: `Bearer ${localStorage.getItem('token')}`
     })
   };
   client: HttpClient;
-  
+  errorMessage: string;
+
   constructor(private http: HttpClient) { 
     this.client = http;
+    this.errorMessage = '';
   }
 
-  getUser(): Observable<User[] | Error | object>{
+  getUser(){
     const url = `${this.baseUrl}`;
-    return this.http.get(url, this.httpOptions).pipe(
-      catchError(
-        (async () => new Error('Error in getUserById'))
-      )
-    );
+    return this.http.get(url, {headers: this.httpOptions.headers}).subscribe(data => {
+      return data;
+    },
+    error =>{
+      this.errorMessage = error.message;
+      console.error('There was an error in getUser request', error);
+      return error;
+    })
   }
 
-  getUserById(id: number): Observable<User | Error | object>{
+  getUserById(id: number){
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get(url, this.httpOptions).pipe(
-      catchError(
-        (async () => new Error('Error in getUserById'))
-      )
-    );
+    return this.http.get<User>(url,{headers: this.httpOptions.headers}).subscribe(data => {
+      return data;
+    },
+    error => {
+      this.errorMessage = error.message;
+      console.error('There was an error in getUserById request', error);
+      return error;
+    })
   }
 
-  login(loginData: loginData): Observable<Auth | Error | object>{
+  login(loginData: loginData){
     const url = `${this.baseUrl}/login`;
-    return this.http.post(url,loginData,this.httpOptions).pipe(
-      catchError(
-        (async () => new Error('Error in Login'))
-      )
-    );
+    return this.http.post<Auth>(url,loginData,{headers: this.httpOptions.headers}).subscribe(data => {
+      if (data.token) {
+        localStorage.setItem('token', data.token);
+        return data;
+      }
+    },
+    error => {
+      this.errorMessage = error.message;
+      console.error('There was an error in Login Request', error);
+      return error;
+    });
   }
+  
 
-  register(registerData: registerData): Observable<User | Error | object>{
+  register(registerData: registerData){
     const url = `${this.baseUrl}/register`;
-    return this.http.post(url,registerData,this.httpOptions).pipe(
-      catchError(
-        (async () => new Error('Error in Register'))
-      )
-    );
+    return this.http.post<User>(url,registerData,{headers: this.httpOptions.headers}).subscribe(data => {
+      return data
+    },
+    error => {
+      this.errorMessage = error.message;
+      console.error('There was an error in Register Request', error);
+      return error;
+    })
   }
 
   // deleteUser(id:number): Observable<any>{
