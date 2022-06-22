a6b953ea2451fed5f5f62cf97ad5ea42cc7cadea
Alexandre COSTA MOREIRA <alexcostamoreira.pro@gmail.com>
2022-06-22 23:56:34 +0200

    Use login

--- app/src/app/components/login/login.component.ts
+++ app/src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Login } from '@models/login.model';
+import { Auth } from '@models/auth.model';
+import { ApiService } from '@services/api-service/api.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-login',
@@ -11,26 +15,32 @@ export class LoginComponent implements OnInit {
 
 	public username: FormControl = new FormControl('', [Validators.required]);
 	public password: FormControl = new FormControl('', [Validators.required]);
+	public apiService: ApiService;
+	public router: Router;
 
-	constructor() {}
+	constructor(_apiService: ApiService, _router: Router) {
+		this.apiService = _apiService;
+    this.router = _router;
+	}
+
+  public ngOnInit(): void {}
 
 	public getErrorMessage(obj: FormControl): string {
-		if(obj.hasError('required'))
-			return "Field required";
+		if (obj.hasError('required')) {
+			return 'Field required';
+		}
 
 		return '';
 	}
 
-	public onSubmit(): void {
-		let data = {
+	public async onSubmit(): Promise<void> {
+		const login: Login = {
 			username: this.username.value,
 			password: this.password.value
 		};
 
-		console.table(data);
-	}
-
-	public ngOnInit(): void {
-
+		const auth: Auth = await this.apiService.login(login);
+		localStorage.setItem('token', auth.token);
+    this.router.navigateByUrl('/home')
 	}
 }
